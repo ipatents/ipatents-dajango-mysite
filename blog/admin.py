cc79d6feaba5868734853fc86a8b91986fb0ecb9
ipatents <32400426+ipatents@users.noreply.github.com>
2017-10-02 16:30:43 -0400

    copied to new place to sync

---
+++ blog/admin.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+#sfrom django.contrib import dianeludin
+from .models import Post
+#admin.site.register(Post)
+
+from django.contrib import admin
+from .models import Post, Comment
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish',
+    'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+admin.site.register(Post, PostAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
+admin.site.register(Comment, CommentAdmin)
